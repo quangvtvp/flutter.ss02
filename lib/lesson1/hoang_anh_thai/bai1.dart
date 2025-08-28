7f091cc3c2d63e3cbf7b8afe868beef711b8d593
ptcutis1tg <thaidepzdai1@gmail.com>
2025-08-28 16:09:12 +0700

    cap nhat bai 1

---
+++ lib/lesson1/hoang_anh_thai/bai1.dart
@@ -0,0 +1,8 @@
+void main() {
+  int age = 16;
+  String name = "hoang anh thai";
+  double height = 1.7;
+  print(
+    'Ten toi la $name, $age toi da years tuoi and chieu cao cua toi la $height met.',
+  );
+}
