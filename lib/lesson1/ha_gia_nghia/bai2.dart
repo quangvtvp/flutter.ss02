049e7bee35f79c43f54ce3ed415bd8474d975d5c
hagianghia218-glitch <hagianghia218@gmail.com>
2025-08-29 14:11:20 +0700

    lam bai2

--- lib/lesson1/ha_gia_nghia/bai2.dart
+++ lib/lesson1/ha_gia_nghia/bai2.dart
@@ -2,9 +2,4 @@ void main () {
   int number1 = 13;
   int number2 = 43;
   print('Tong =${number1 + number2} ,Hieu =${number2 - number1} , Tich =${number1 * number2} , Thuong =${number2 / number1}');
-  String name ='Dinh tien Hoang';
-  int age = 18;
-  double gpa = 3.7;
-  String mssv ='HS0294';
-  print('Tên : $name , Tuổi : $age , Điểm trung bình : $gpa , MSSV : $mssv');}
-  
+}
