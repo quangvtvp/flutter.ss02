027f061e61700ea170430b6cdee8e4b96a0eee7f
Van Duong <duongpvhoccntt@gmail.com>
2025-09-01 07:39:09 +0700

    Bai so 1

---
+++ lib/lesson1/Pham_Van_Duong/BaiTapVeNha/Baiso3.dart
@@ -0,0 +1,9 @@
+void main() {
+  String Hovaten = 'Pham Van Duong';
+  int tuoi = 16;
+  double TB = 8.5;
+  String gioitinh = 'Nam';
+  String SBD = '06';
+  print(
+      'Hoc sinh: $Hovaten, Tuoi: $tuoi, GPA: $TB, Gioi tinh: $gioitinh, SBD: $SBD');
+}
