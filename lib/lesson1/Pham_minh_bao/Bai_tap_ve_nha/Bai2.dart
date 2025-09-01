e18200a28d019e02237590d4b764b090d314fdaa
Minhbaooo <pham2inhbao1308@gmail.com>
2025-09-01 10:54:33 +0700

    Bài tập về nhà Lesson2

---
+++ lib/lesson1/Pham_minh_bao/Bai_tap_ve_nha/Bai2.dart
@@ -0,0 +1,14 @@
+//Bài 2 – Tính toán
+//Khai báo number1, number2.
+//Tính tổng, hiệu, tích, thương.
+//In kết quả bằng interpolation.
+
+void main() {
+  double number1 = 13;
+  double number2 = 8.2;
+  double tong = number1 + number2;
+  double hieu = number1 - number2;
+  double tich = number1 * number2;
+  double thuong = number1 / number2;
+  print(' hai số $number1 và $number2 có: Tổng = $tong, hiệu = $hieu, tích = $tich, thương = $thuong.');
+}
