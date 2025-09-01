3928e3da26aedb2d6b24cab1cac4b9f55241c5cc
cuong240904 <cuong240904@gmail.com>
2025-09-01 23:22:49 +0700

    homework

---
+++ lib/lesson1/dongvietcuong/bai5.dart
@@ -0,0 +1,18 @@
+void main() {
+  double math = 9, english = 7.3, literature = 5.5;
+  double avg = (math + english + literature) / 3;
+  double gpa = avg / 2.5;
+  String grade;
+  if (avg >= 8) {
+    grade = "A";
+  } else if (avg >= 6.5) {
+    grade = "B";
+  } else if (avg >= 6.5) {
+    grade = "C";
+  }else if (avg >= 5) {
+    grade = "D";
+  }else {
+    grade = "F";
+    }
+   print('Điểm TB: ${avg.toStringAsFixed(2)}, GPA: ${gpa.toStringAsFixed(2)}, Điểm chữ: $grade');
+}
