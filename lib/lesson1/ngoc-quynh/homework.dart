8395f475cebfcaf359bc7a1bd5d1749c3f86225f
Ngoc Quynh <ngocquynhnguyen301209@gmail.com>
2025-09-04 00:35:56 +0700

    bài1.dart

--- lib/lesson1/ngoc-quynh/homework.dart
+++ lib/lesson1/ngoc-quynh/homework.dart
@@ -1,6 +1,6 @@
 void main() {
-int age = 15;
-var name = "Ngoc Quynh";
-double height = 1.55;
-print('xin chào, tôi tên là $name, tôi $age tuổi và cao $height mét');
+  int age = 15;
+  var name = "Ngoc Quynh";
+  double height = 1.55;
+  print('xin chào, tôi tên là $name, tôi $age tuổi và cao $height mét');
 }
