cb5ba8082954f5c099be9c3f292da39a47370b15
HaDuongChau <haduongchau120309@gmail.com>
2025-09-04 21:32:22 +0700

    hdchau

---
+++ lib/lesson1/ha_duong_chau/homework.dart/exercise3.dart
@@ -0,0 +1,9 @@
+void main() {
+  String fullName = 'Ha Duong Chau';
+  int age = 16;
+  double gpa = 8.7;
+  String gender = 'Nam';
+  String studentId = 'C12345678';
+  print(
+      'Sinh vien: $fullName, Tuoi: $age, GPA: $gpa, Gioi tinh: $gender, MSSV: $studentId');
+}
