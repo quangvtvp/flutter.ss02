920a881fe928299b4351926b5c89541da9421395
Tran Toan <trtoanx2@gmail.com>
2025-09-06 20:23:44 +0700

    Toan'BTVNLesson2

---
+++ lib/lesson1/Toan_Tran/bonus.dart
@@ -0,0 +1,12 @@
+// Bonus : Sử dụng list<double> và for//
+void main() {
+  List<String> monhoc = ["Toán", "Lý", "Hóa", "Sinh", "Tin"];
+  List<double> diem = [10, 10, 9.9, 9, 9.2];
+  double tong = 0;
+  for (int i = 0; i < monhoc.length; i++) {
+    print("${monhoc[i]}: ${diem[i]}");
+    tong = tong + diem[i];
+  }
+  double diemTB = tong / diem.length;
+  print("Điểm trung bình: $diemTB");
+}
