b4e5a3c2c1c8eee167c42544d9f0324aacbfd8e4
nguyentuanminh <i4330134@gmail.com>
2025-09-06 18:34:53 +0700

    btvn

--- lib/nguyen_tuan_minh/Baithem.dart
+++ lib/nguyen_tuan_minh/Baithem.dart
@@ -5,7 +5,7 @@ void main() {
   double Ly = 9;
   double Sinh = 9;
   List<double> Diem = [Toan, Van, Anh, Ly, Sinh];
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 4; i++) {
     print("Điểm TB: ${Diem.reduce((a, b) => a + b) / Diem.length}");
   }
 }
