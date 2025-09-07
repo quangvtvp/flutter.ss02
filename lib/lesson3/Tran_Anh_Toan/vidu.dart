1206121cda59064e56060f4980fbb892c236d2a4
Tran Toan <trtoanx2@gmail.com>
2025-09-07 21:54:21 +0700

    vidu123456

--- lib/lesson3/Tran_Anh_Toan/vidu.dart
+++ lib/lesson3/Tran_Anh_Toan/vidu.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
 
 void main() {
-  print('Xin chào bạn!');
   print('Nhập giờ hiện tại: ');
   double? hour = double.tryParse(stdin.readLineSync() ?? '');
   if (hour == null || hour < 0 || hour > 24) {
     print('Giờ không hợp lệ, thử lại.');
     return;
   }
-  if (hour < 11) {
+  if (hour < 12) {
     print('Chào buổi sáng');
-  } else if (hour < 19) {
+  } else if (hour < 18) {
     print('Chào buổi chiều');
   } else {
     print('Chào buổi tối');
