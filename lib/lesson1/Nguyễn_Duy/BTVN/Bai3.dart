9f7c915b1a86f18eefe4a839e12170d6f149afe2
duycutecun <Duycutecun@gmail.com>
2025-09-07 17:14:20 +0700

    bài 1

---
+++ lib/lesson1/Nguyễn_Duy/BTVN/Bai3.dart
@@ -0,0 +1,12 @@
+void main() {
+  String hoten = 'Nguyễn Khương Duy';
+  int tuoi = 16;
+  double diemtb = 8.5;
+  String gioitinh = 'Nam';
+  int mssv = 08092009;
+  print('Họ tên : $hoten');
+  print('Tuổi : $tuoi');
+  print('Điểm trung bình : $diemtb');
+  print('Giới tính ; $gioitinh');
+  print('MSSV : $mssv');
+}
