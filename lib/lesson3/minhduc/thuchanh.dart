8deb55bff638e71a6c8f5bde28e380d8f1349443
leemingduc <duca2k52cva@gmail.com>
2025-09-07 20:52:23 +0700

    hi

--- lib/lesson3/minhduc/thuchanh.dart
+++ lib/lesson3/minhduc/thuchanh.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
 void main() {
+  print('Xin chào bạn!');
   print('Nhập giờ hiện tại: ');
   int? hour = int.tryParse(stdin.readLineSync() ?? '');
   if (hour == null || hour < 0 || hour > 23) {
     print('Giờ không hợp lệ.');
     return;
   }
-  if (hour < 12) {
+  if (hour < 11) {
     print('Chào buổi sáng');
-  } else if (hour < 18) {
+  } else if (hour < 19) {
     print('Chào buổi chiều');
   } else {
     print('Chào buổi tối');
