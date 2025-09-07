5610016e74240ce40e22cb5ff6708d1936dbeddb
Duc Dung <dukdunx09@gmail.com>
2025-09-07 09:06:37 +0700

    Bai 1

---
+++ lib/lesson1/Bui_Duc_Dung/B3.dart
@@ -0,0 +1,9 @@
+void main() {
+  String fullName = 'Bui Duc Dung';
+  int age = 16;
+  double gpa = 9.2;
+  String gender = 'Nam';
+  String studentId = '202512345';
+  print(
+      'Sinh viên: $fullName, Tuổi: $age, GPA: $gpa, Giới tính: $gender, MSSV: $studentId');
+}
---
+++ lib/lesson1/Bui_Duc_Dung/B5.dart
@@ -0,0 +1,18 @@
+void main() {
+  double Toan = 9.6, Ly = 9.4, Hoa = 9.5;
+
+  double avg = (Toan + Ly + Hoa) / 3;
+  double gpa = avg / 2.5;
+
+  String grade = (avg >= 8.5)
+      ? 'A'
+      : (avg >= 7.0)
+          ? 'B'
+          : (avg >= 5.5)
+              ? 'C'
+              : (avg >= 4.0)
+                  ? 'D'
+                  : 'F';
+  print(
+      'Điểm trung bình: ${avg.toStringAsFixed(2)}, GPA: ${gpa.toStringAsFixed(2)}, Xếp loại: $grade');
+}
