ffb7db899f98e215581ac82c885258c7f76f380c
Lê Uyên Nhi <nhile2kk9@gmail.com>
2025-09-14 16:55:22 +0700

    Vd1bai3

--- lib/lesson3/Le_Uyen_Nhi/Vd1.dart
+++ lib/lesson3/Le_Uyen_Nhi/Vd1.dart
@@ -1,17 +1,15 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Nhập giờ hiện tại (0-23): ');
+  print('Nhập giờ (0–23): ');
   int? hour = int.tryParse(stdin.readLineSync() ?? '');
-
   if (hour == null || hour < 0 || hour > 23) {
-    print('Giờ không hợp lệ!');
+    print('Giờ không hợp lệ.');
     return;
   }
-
-  if (hour >= 0 && hour < 12) {
+  if (hour < 12) {
     print('Chào buổi sáng');
-  } else if (hour >= 12 && hour < 18) {
+  } else if (hour < 18) {
     print('Chào buổi chiều');
   } else {
     print('Chào buổi tối');
