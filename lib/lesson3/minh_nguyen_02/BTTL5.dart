783f5f72db156a01104ec67aefeefa91a5491566
Minh Nguyen <nmnguyen2506@gmail.com>
2025-09-14 08:53:50 +0700

    add bttl5

--- lib/lesson3/minh_nguyen_02/BTTL5.dart
+++ lib/lesson3/minh_nguyen_02/BTTL5.dart
@@ -7,7 +7,7 @@ void main() {
     print('mat khau qua ngan');
     return;
   }
-  if (mk!.contains(RegExp(r'[0-9]')) && mk!.contains(RegExp(r'[A-Z]'))) {
+  if (mk.contains(RegExp(r'[0-9]')) && mk.contains(RegExp(r'[A-Z]'))) {
     print('mat khau hop le');
   } else {
     print('mat khau phai co it nhat 1 so va 1 chu cai in hoa');
