2821419d22f8e132aa4d6eeda9bc6c12f3e5663d
janyrollzone <baodimine@gmail.com>
2025-09-27 22:30:38 +0700

    tranbao

---
+++ lib/lesson4/tran_bao/bttl4.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Nhập số nguyên dương n: ');
+  final raw = stdin.readLineSync() ?? '';
+  final n = int.tryParse(raw);
+
+  if (n == null || n <= 0) {
+    print('Nhập số nguyên dương');
+    return;
+  }
+
+  int u1 = 0;
+  int u2 = 1;
+
+  print('Dãy fibonaci:');
+  for (int i = 1; i <= n; i++) {
+    print(u1);
+    int u3 = u1 + u2;
+    u1 = u2;
+    u2 = u3;
+  }
+}
