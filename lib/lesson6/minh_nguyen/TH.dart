cc8e866f64d18b2e0624436983c57054ca0af8ea
Minh Nguyen <nmnguyen2506@gmail.com>
2025-09-28 10:39:09 +0700

    add bt

--- lib/lesson6/minh_nguyen/TH.dart
+++ lib/lesson6/minh_nguyen/TH.dart
@@ -3,4 +3,7 @@ void main() {
   print(' mon dau tien la:${food[0]}');
   food.add('bun');
   print('tong so mon an la:${food.length}');
+  food.remove(90);
+  food.removeAt(4 - 1);
+  print('danh sach mon an la:$food');
 }
