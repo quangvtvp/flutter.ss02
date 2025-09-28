440d88ae44ea1a00397d5bca148e53a6b7f41d20
duycutecun <Duycutecun@gmail.com>
2025-09-28 10:54:53 +0700

    kkk

--- lib/lesson6/Nguyễn_Duy/VD_2.dart
+++ lib/lesson6/Nguyễn_Duy/VD_2.dart
@@ -2,25 +2,22 @@ void main() {
   var diem = [9, 7, 8, 6, 5];
   var max = diem[0];
   var min = diem[0];
-  for (int i = 1; i < diem.length; i++) {
+  for (int i = 0; i < diem.length; i++) {
     if (diem[i] > max) {
       max = diem[i];
-      for (int i = 0; i < diem.length; i++) {
-        if (diem[i] < min) {
-          min = diem[i];
-        }
-        print('điểm cao nhất là: $max');
-        print('điểm thấp nhất là: $min');
-        {
-          diem.add(10);
-          print('các điểm là: $diem');
-          for (int diemtong in diem) {
-            diemtong += diemtong;
-            var avg = diemtong / diem.length;
-            print('điểm trung bình là: $avg');
-          }
-        }
-      }
+    }
+    if (diem[i] < min) {
+      min = diem[i];
     }
   }
+  print('Điểm cao nhất là: $max');
+  print('Điểm thấp nhất là: $min');
+  diem.add(10);
+  print('Các điểm sau khi thêm là: $diem');
+  var tongdiem = 0;
+  for (int d in diem) {
+    tongdiem += d;
+  }
+  var avg = tongdiem / diem.length;
+  print('Điểm trung bình là: $avg');
 }
