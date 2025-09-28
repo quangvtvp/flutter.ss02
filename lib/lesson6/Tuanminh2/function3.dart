306268fe062b64bb26667df2981bd106fb631497
nguyentuanminh <i4330134@gmail.com>
2025-09-28 16:17:30 +0700

    Tuan Minh

---
+++ lib/lesson6/Tuanminh2/function3.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  print("Nhập vào 1 từ:");
+  String word = stdin.readLineSync()!;
+  var list = word.split('');
+  for (var i = 0; i < list.length; i++) {
+    stdout.write('*');
+  }
+  List<String> list1 = List.filled(list.length, '*');
+  print('nhập 1 kí tự:');
+  String char = stdin.readLineSync()!;
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].toLowerCase() == char.toLowerCase()) {
+      list1[i] = char;
+    }
+  }
+  print(list1.join());
+}
