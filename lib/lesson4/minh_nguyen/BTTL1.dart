3b4553be7dec257757ee1ced8c2b2218cd821e8c
Minh Nguyen <nmnguyen2506@gmail.com>
2025-09-28 08:55:39 +0700

    bttl1- da fix loi tai lop

--- lib/lesson4/minh_nguyen/BTTL1.dart
+++ lib/lesson4/minh_nguyen/BTTL1.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   for (var j = 1; j < 10; j++) {
     for (var i = 1; i < 10; i++) {
-      stdout.write('$i x $j=${i * j}  ');
+      stdout.write('$i x $j=${i * j} \t');
     }
     print('');
   }
