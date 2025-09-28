c0be7bbf8a8eec7f4ee2f777edd3ba8151fc9ab8
cuong240904 <cuong240904@gmail.com>
2025-09-28 13:53:39 +0700

    sửa thêm

--- lib/lesson6/cuong/test.dart
+++ lib/lesson6/cuong/test.dart
@@ -1,7 +1,23 @@
 import 'dart:io';
 
 void main() {
-  List<String> names = ['a', 'b', 'c', 'd'];
+    List<String> names = [];
+  stdout.write("Nhập số lượng bạn trong lớp: ");
+  int? n = int.tryParse(stdin.readLineSync() ?? '');
+  if (n == null || n <= 0) {
+    print("Số lượng không hợp lệ!");
+    return;
+  }
+  for (var i = 0; i < n; i++) {
+    stdout.write("Nhập tên bạn thứ ${i + 1}: ");
+    String? name = stdin.readLineSync();
+    if (name != null && name.isNotEmpty) {
+      names.add(name);
+    } else {
+      names.add("Không tên");
+    }
+  }
+
   List<Map<String, String>> students = [];
   for (var i = 0; i < names.length; i++) {
     stdout.writeln("hay nhap so thich cua ${names[i]}:");
