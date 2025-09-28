5a7fc64245035f77a46798fcb67bae8a1ae3cfb9
cuong240904 <cuong240904@gmail.com>
2025-09-28 15:45:02 +0700

    a

--- lib/lesson6/cuong/trochoi.dart
+++ lib/lesson6/cuong/trochoi.dart
@@ -1,19 +1,29 @@
 import 'dart:io';
 
 void main() {
-  String input = "hoc bai";
-  play(input);
+  List<String> words = ["hoc bai", "an com", "choi game", "di ngu"];
+  List<String> word = words..shuffle();
+  for (var i = 0; i < word.length; i++) {
+    play(word[i]);
+  }
 }
 
 void play(String input) {
   List<String> chars = input.split('');
   chars.shuffle();
-  print("hãy sắp xếp lại các chữ cái trên thành chữ cái hoàn chỉnh :  $chars");
-  stdout.write("nhập chữ cái bạn đoán: ");
+  print("Hãy sắp xếp lại các chữ cái để thành từ đúng: $chars");
+  stdout.write("Nhập chữ bạn đoán: ");
   String? guess = stdin.readLineSync();
- while (guess != input) {
-    stdout.write("bạn đoán sai rồi, hãy nhập lại: ");
+  var i = 0;
+  while (guess == null) {
+    stdout.write("Vui lòng nhập chữ bạn đoán: ");
     guess = stdin.readLineSync();
   }
-  print("bạn đã đoán đúng");
+  if (guess.toLowerCase() == input) {
+    i = i + 20;
+    print("Chính xác! bạn được thêm $i điểm");
+  } else {
+    print("Sai rồi! Đáp án đúng là: $input");
+  }
+  print("Tổng điểm của bạn là: $i");
 }
