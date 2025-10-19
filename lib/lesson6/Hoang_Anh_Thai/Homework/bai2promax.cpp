072a2092f3e1ca4527f56ff27468d18b72692e60
ptcutis1tg <thaidepzdai1@gmail.com>
2025-10-19 20:12:41 +0700

    ukii

--- lib/lesson6/Hoang_Anh_Thai/Homework/bai2promax.cpp
+++ lib/lesson6/Hoang_Anh_Thai/Homework/bai2promax.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+void dongucaobang(){
+    int a=36;
+    cout<<a;
+}
 int main(){
-    
+    int m,n;
+    std::cin>>m>>n;
+    vector<int> a(n);
+    for(int i = 0; i<n;i++){
+        int a;
+    }
+    for(int i = 0; i<n;i++){
+
+    }
 }
---
+++ lib/lesson7/Hoang_Anh_Thai/try1.dart
@@ -0,0 +1,29 @@
+// ignore: file_names
+void main() {
+  var maphocsinh = [
+    {
+      'tacgia': 'Nguyen van A',
+      'monhoc': 'Toan',
+      'tensach': 'Giai tich 12',
+      'sotrang': 500
+    },
+    {
+      'tacgia': 'Tran thi B',
+      'monhoc': 'Ly',
+      'tensach': 'Vat ly 12',
+      'sotrang': 450
+    },
+    {
+      'tacgia': 'Le van C',
+      'monhoc': 'Hoa',
+      'tensach': 'Hoa hoc 12',
+      'sotrang': 400
+    },
+    {
+      'tacgia': 'Pham thi D',
+      'monhoc': 'Sinh',
+      'tensach': 'Sinh hoc 12',
+      'sotrang': 350
+    },
+  ];
+}
