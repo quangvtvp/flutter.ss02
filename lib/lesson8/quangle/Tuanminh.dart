3dc9d3b401a75b55c2f5e002409b3e6be0910d56
nguyentuanminh <i4330134@gmail.com>
2025-10-26 20:50:16 +0700

    tuanminh

--- lib/lesson8/quangle/Tuanminh.dart
+++ lib/lesson8/quangle/Tuanminh.dart
@@ -4,16 +4,13 @@ class Student {
 
   Student(this.name, this.yearbirth);
 
-  Student.origin(this.name) {
-    yearbirth = 2008;
-  }
+  Student.origin(this.name) : yearbirth = 2008;
 
-  Student.origin1(this.name) {
-    yearbirth = 2009;
-  }
+  Student.origin1(this.name) : yearbirth = 2009;
 }
 
 void main() {
   var student = Student.origin("minh");
-  print(student);
+  print(student.name);
+  print(student.yearbirth);
 }
