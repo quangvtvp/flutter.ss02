427a565254522ff510046c65c5ef6ca91a3838d7
Van Duong <duongpvhoccntt@gmail.com>
2025-10-26 21:41:33 +0700

    lambt

--- lib/lesson8/PhamVanDuong/Classgrades.dart
+++ lib/lesson8/PhamVanDuong/Classgrades.dart
@@ -6,7 +6,7 @@ class StudentGrades {
 
   StudentGrades(this.studentName);
 
-  bool _validateScore(double score) {
+  bool validateScore(double score) {
     if (score < 0 || score > 10) {
       print('không hợp lệ điểm phải từ 0 đến 10');
       return false;
@@ -18,7 +18,7 @@ class StudentGrades {
     return (_mathScore + _physicsScore + _chemistryScore) / 3;
   }
 
-  String _getGrade(double average) {
+  String _GetGrade(double average) {
     if (average >= 8.0) return 'Giỏi';
     if (average >= 6.5) return 'Khá';
     if (average >= 5.0) return 'Trung bình';
@@ -29,7 +29,7 @@ class StudentGrades {
 void main() {
   var student = StudentGrades('Pham Van Duong');
   var average = student._CalculateAverage();
-  var grade = student._getGrade(average);
+  var grade = student._GetGrade(average);
   print(
       'Tên học sinh: ${student.studentName}, Điểm trung bình: ${average.toStringAsFixed(2)}, Học lực: $grade');
 }
