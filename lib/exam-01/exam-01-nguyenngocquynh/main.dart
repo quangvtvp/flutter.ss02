9a9005bc4972ce5beec43b959f32385be5ccb7b5
Ngoc Quynh <ngocquynhnguyen301209@gmail.com>
2025-11-09 11:07:40 +0700

    bt

---
+++ lib/exam-01/exam-01-nguyenngocquynh/main.dart
@@ -0,0 +1,29 @@
+ import 'dart:io';
+ void main() {
+  List<String> books = [
+    'Dê ́ Mèn phiêu lưu ký',
+    'Tăt đèn',
+
+    'Sô ́ đo ',
+    'Chí Phèo',
+    'Lão Hạc',
+    'Nhà giả kim',
+    'Đă ́c nhân tâm',
+    'Tuô ̉i tre ̉ đáng giá bao nhiêu',
+    'Cây cam ngọt của tôi',
+
+
+    'Sapiens - Lược sử loài người',
+  ];
+  for (var book in books) {
+    print(book);
+  }
+   print(' Tong so sach: ${books.length} ');
+}
+
+ double maxprice = 0;
+ for (var book in books)
+
+
+
+
