dfd7db9ecb2a4f205b494f734998e05566778ae6
nguyenvietthuc1001 <kiuuvn@gmail.com>
2025-11-09 09:53:23 +0700

    book

---
+++ lib/exam-01/exam-01-nguyenvietthuc/book.dart
@@ -0,0 +1,39 @@
+void main() {
+  var book1 = Book('Dế Mèn phiêu lưu ký', 'Tô Hoài', 1941, 50000, true);
+  print(book1.getInfo());
+
+  book1.borrow();
+  print(book1.getInfo());
+
+  print(book1.isOldBook());
+}
+
+class Book {
+  String title;
+  String author;
+  int publishYear;
+  double price;
+  bool isAvailable = true;
+
+  Book(this.title, this.author, this.publishYear, this.price, this.isAvailable);
+
+  void borrow() {
+    this.isAvailable = false;
+  }
+
+  void returnBook() {
+    this.isAvailable = true;
+  }
+
+  String getInfo() {
+    return "Tên: $title, Tác giả: $author, Năm: $publishYear, Giá: $price\đ, Trạng thái: ${this.isAvailable ? "Có sẵn" : "Đã mượn"}";
+  }
+
+  bool isOldBook() {
+    if (this.publishYear < 1950) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
