4fa58ada51feca4ec7f7721d23562575bc5eecc5
leemingduc <duca2k52cva@gmail.com>
2025-11-09 14:41:48 +0700

    sửa

--- lib/lesson11/minhduc/vd3.dart
+++ lib/lesson11/minhduc/vd3.dart
@@ -5,9 +5,11 @@ Future<void> main(List<String> args) async {
   // Dùng await để chờ Future hoàn thành và lấy giá trị.
   // Code trông giống như code đồng bộ!
   var ketQua = await layDuLieuoto();
+  var latestModel = await getLatestModel();
 
   // Dòng này chỉ được thực thi sau khi await ở trên hoàn thành.
   print('Kết quả nhận được: $ketQua');
+  print('Ô tô cũ nhất đã được tìm thấy là: $latestModel');
   print('Chương trình kết thúc.');
 }
 
@@ -31,4 +33,15 @@ Future<String> layDuLieuoto() async {
     return 'Danh sách ô tô đã được sắp xếp theo năm: ${cars.map((car) => '${car.model} (${car.year})').join(', ')}';
   });
   
+}
+Future<String> getLatestModel() async {
+  List<Car> cars = [
+    Car('Toyota', 2020),
+    Car('Honda', 2022),
+    Car('Ford', 2021),
+  ];
+  return Future.delayed(Duration(seconds: 15), () {
+    cars.sort((a, b) => b.year.compareTo(a.year));
+    return cars.first.model;
+  });
 }
