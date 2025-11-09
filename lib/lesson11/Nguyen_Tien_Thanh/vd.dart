f51fcca547e65fca7d7636c9682c8bbaeae22d4d
tien thanh <phamthithuy17121978@gmail.com>
2025-11-09 14:39:01 +0700

    bai 1

---
+++ lib/lesson11/Nguyen_Tien_Thanh/vd.dart
@@ -0,0 +1,24 @@
+Future<void> main(List<String> args) async {
+  print('Start the app');
+  print('fetching the latest model');
+  var model = await getLatestModel();
+  print('Latest model:$model');
+  print('do other work');
+} 
+class Car {
+  String model;
+  int year;
+  Car(this.model,this.year);
+}
+Future<String> getLatestModel() async {
+  List<Car> models = [
+    Car('Toyota', 2020),
+    Car('Honda', 2021),
+    Car('Ford', 2022),
+    Car('Chevrolet', 2023),
+  ];
+  return Future.delayed(Duration(seconds: 1000), () {
+    models.sort((a, b) => b.year.compareTo(a.year));
+    return models.first.model;
+  });
+}
