0ee1b5a5233263ea960569e954af4bbceba56aa4
leemingduc <duca2k52cva@gmail.com>
2025-11-23 20:55:39 +0700

    .

---
+++ flutter/lesson13/minhduc/lib/my_game_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class MyGameScreen extends StatelessWidget {
+  const MyGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Game Screen'),
+        backgroundColor: const Color.fromARGB(255, 41, 84, 158),
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to My Game!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
