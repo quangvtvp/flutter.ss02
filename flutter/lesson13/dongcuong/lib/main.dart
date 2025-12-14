9b08fdd4eb689355d48a87f6c22d3ea3caf320d2
cuong240904 <cuong240904@gmail.com>
2025-12-14 19:41:21 +0700

    .

--- flutter/lesson13/dongcuong/lib/main.dart
+++ flutter/lesson13/dongcuong/lib/main.dart
@@ -1,18 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:dongcuong/hinh.dart';
 
 void main() {
-  runApp(const MyGame());
+  runApp(const MyApp());
 }
 
-class MyGame extends StatelessWidget {
-  const MyGame({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyGameScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyGameScreen());
   }
 }
