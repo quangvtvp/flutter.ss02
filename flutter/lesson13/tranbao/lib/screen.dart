cafdee34b5895c1b41cbec2bf29f58f56cb284e2
janyrollzone <baodimine@gmail.com>
2025-12-14 19:58:38 +0700

    tranbao

---
+++ flutter/lesson13/tranbao/lib/screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyGameScreen extends StatelessWidget {
+  const MyGameScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Game Screen'),
+        backgroundColor: const Color.fromARGB(255, 10, 10, 10),
+        foregroundColor: Colors.white,
+      ), 
+
+      body: const Center(
+        child: Text('Welcome to My Game!'),
+      ), 
+    ); 
+  }
+}
