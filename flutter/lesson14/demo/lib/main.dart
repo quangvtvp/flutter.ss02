292a21601bdc37e45fdd30edd44c5e660704aa2e
Nguyen Duc Thuan <nguyenducthuan24@gmail.com>
2026-01-10 14:24:48 +0700

    resolve conflict

--- flutter/lesson14/demo/lib/main.dart
+++ flutter/lesson14/demo/lib/main.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import 'package:lesson14_member_funny_game/PhamVanDuong/member_screen_v1_stateless_form.dart';
-import 'package:lesson14_member_funny_game/thuannd/member_screen_v1_stateless_form.dart'
-    hide MemberFunnyGameScreenV1;
-=======
-import 'package:lesson14_member_funny_game/thuannd/lesson-13-stateful/color_picker.dart';
-import 'package:lesson14_member_funny_game/thuannd/lesson-13-stateful/lightbuld.dart';
-import 'package:lesson14_member_funny_game/thuannd/lesson-14-listview-gridview/color-selector.dart';
-import 'package:lesson14_member_funny_game/thuannd/lesson-14-listview-gridview/grid-game.dart';
 import 'package:lesson14_member_funny_game/thuannd/lesson-14-listview-gridview/simple_grid.dart';
-import 'package:lesson14_member_funny_game/thuannd/lesson-14-listview-gridview/simple_horizontal_list.dart';
-import 'package:lesson14_member_funny_game/thuannd/member_screen_v1_stateless_form.dart';
-import 'package:lesson14_member_funny_game/thuannd/member_screen_v2_stateless_list.dart';
->>>>>>> 7dd4b014e98195ab1484d14944c75eb05c7f46c7
-import 'package:lesson14_member_funny_game/thuannd/member_screen_v5_gemini.dart';
 
 import 'thuannd/lesson-14-listview-gridview/simplelistview.dart';
 
@@ -41,7 +25,7 @@ class MemberFunnyGameApp extends StatelessWidget {
       // home trỏ tới màn hình game chính đã tách riêng trong folder screens
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter Demo')),
-        body: const MyWidget(),
+        body: const SimpleGridView(),
       ),
     );
   }
