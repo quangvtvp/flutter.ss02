705c6d1095559ef0ee0b6b861e15c3f9b9900c9c
leemingduc <duca2k52cva@gmail.com>
2026-01-25 19:18:14 +0700

    .

--- flutter/lesson14/demo/lib/leduc/main.dart
+++ flutter/lesson14/demo/lib/leduc/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lesson14_member_funny_game/leduc/cardgame.dart';
 
-import 'package:lesson14_member_funny_game/thuannd/lesson-15-mini-game/memory_game_v4_sound.dart';
-
-// HÀM main là điểm bắt đầu của ứng dụng Flutter
 void main() {
   runApp(const MemberFunnyGameApp());
 }
@@ -22,7 +20,7 @@ class MemberFunnyGameApp extends StatelessWidget {
       //   useMaterial3: true,
       // ),
       // home trỏ tới màn hình game chính đã tách riêng trong folder screens
-      home: const MemoryGameV4Sound(),
+      home: const MycardGame1(),
     );
   }
 }
