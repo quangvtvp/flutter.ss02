309b534549ff3e541a4a172641bc6dc618f3c97c
leemingduc <duca2k52cva@gmail.com>
2026-01-25 19:34:50 +0700

    .

--- flutter/lesson14/demo/lib/main.dart
+++ flutter/lesson14/demo/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 
 import 'package:lesson14_member_funny_game/thuannd/lesson-15-mini-game/memory_game_v4_sound.dart';
-=======
 import 'package:lesson14_member_funny_game/thuannd/lesson-14-listview-gridview/simplelistview.dart';
 import 'package:lesson14_member_funny_game/thuannd/lesson-15-mini-game/student-name/my_card_game.dart';
 import 'package:lesson14_member_funny_game/thuannd/sqlight/todo.dart';
 import 'package:lesson14_member_funny_game/thuannd/sqlight/todo_v2.dart';
->>>>>>> 31bb263188037c981d0faa1336b91557a5fd55a5
 
 // HÀM main là điểm bắt đầu của ứng dụng Flutter
 void main() {
@@ -29,11 +26,8 @@ class MemberFunnyGameApp extends StatelessWidget {
       //   useMaterial3: true,
       // ),
       // home trỏ tới màn hình game chính đã tách riêng trong folder screens
-<<<<<<< HEAD
       home: const MemoryGameV4Sound(),
-=======
       home: const TodoListV2(),
->>>>>>> 31bb263188037c981d0faa1336b91557a5fd55a5
     );
   }
 }
